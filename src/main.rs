53577cc6d34e1294781bc2b69edc791c3f2a6f86
Elazrod56 <thomas.lf5629@gmail.com>
2023-04-30 23:58:49 +0200

    comments added

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,18 @@ use std::fs;
 use std::io;
 
 fn main() -> io::Result<()> {
+    // The main function must return a Result<()> if we want to use the '?' operator on lines 13 and 14
+
     println!("-------- THE TIKTOK JSON ANALYZER --------");
     println!("This program calculates some statistics using your TikTok JSON data export");
-    println!("Please make sure the JSON file is located in json/user_data.json\n");
+    println!("Please make sure the JSON file is located in json/user_data.json \n");
 
     let file = fs::read_to_string("json/user_data.json")?;
     let data = serde_json::from_str::<Value>(file.as_str())?;
 
     let username = &data["Profile"]["Profile Information"]["ProfileMap"]["userName"];
-    println!("File detected \u{2705} \nThe data of {username} will be analyzed...\n");
+    println!("File detected \u{2705}
+        \nThe data of {username} will be analyzed... \n");
 
     // Amount of logins
     println!("\n-------- LOGINS \u{2386} --------\n");
@@ -28,6 +31,7 @@ fn main() -> io::Result<()> {
         "You launched TikTok {} times a day on average\n",
         login_history_len / 183
     );
+    // 6 months corresponds to roughly 183 days, this is why we divide by 183
 
     // Amount of videos watched
     let watched_videos = &data["Activity"]["Video Browsing History"]["VideoList"];
@@ -69,6 +73,7 @@ fn main() -> io::Result<()> {
         .unwrap_or(0);
 
     let date_of_8000th_liked_vid = &liked_videos[liked_videos_len - 1]["Date"];
+    // TikTok only keeps track of your latest 8000 likes. (at least in the data export)
     println!("You liked 8000 videos since {date_of_8000th_liked_vid}\n");
 
     // Comments
