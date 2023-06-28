9c0c67f50df1b6133d6ec95b97af1a07123be7da
Elazrod56 <thomas.lf5629@gmail.com>
2023-06-28 18:32:06 +0200

    Improved comments

--- src/read_file.rs
+++ src/read_file.rs
@@ -2,12 +2,12 @@
 // read_file.rs is in charge of :
 // - reading the user_data.json file and transform it into a String
 // - transforming the String into a serde_json::Value object
-// the program is exited if there is any kind of error (such as file not found - the most likely to occur)
+// the program panics if there is any kind of error (such as file not found - the most likely to occur)
 
 use serde_json::{Error, Value};
 use std::fs;
 
-// these need to be public for the test functions to access them
+// these need to be public for the main function to access them
 pub fn file_into_str(file_path: &str) -> String {
     let file = fs::read_to_string(file_path).unwrap_or_else(|err| {
         eprintln!("Error reading the file: {:?}", err);
