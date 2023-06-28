c2f6a27deff92aa25c6feff600b942aac2645137
Elazrod56 <thomas.lf5629@gmail.com>
2023-06-28 18:31:42 +0200

    Modified UI

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 // main.rs
 // main.rs is in charge of :
+// - calling the functions in read_file.rs to get the data from the JSON file
 // - calling the lib.rs "build" function with the data as an argument -
 // that will build an instance of the "Statistics" struct which contains the processed data
 // - print the data to the console in a user-friendly way
@@ -26,69 +27,71 @@ fn main() {
 
     let statistics = Statistics::build(data);
     println!(
-        "The data of {} has been analyzed. Here are the results",
+        "The data of {} has been analyzed. Results :",
         statistics.username
     );
 
     println!("\n---------- LOGINS \u{1F511} ----------");
     println!(
-        "In the last {} days, you logged in {} times to TikTok.",
+        "In the last {} days, you've launched TikTok {} times",
         statistics.logins.get("Days since 1st login").unwrap(),
         statistics.logins.get("Openings").unwrap()
     ); // it's safe to unwrap because there would have been an error sooner if something was wrong
        // it's also much more concise in terms of code length
     println!(
-        "You opened TikTok {} times a day on average",
-        statistics.logins.get("Openings a day").unwrap()
+        "- {} launches per day on average",
+        statistics.logins.get("Launches per day").unwrap()
     );
 
     println!("\n---------- VIDEO CONSUMPTION \u{1F4FA} ----------");
     println!(
-        "In the last {} days, you have watched {} videos.",
+        "In the last {} days, you've watched {} videos",
         statistics.watched.get("Days since 1st video").unwrap(),
         statistics.watched.get("Videos watched").unwrap()
     );
     println!(
-        "You watched {} videos per day on average",
+        "- {} watched videos per day on average",
         statistics.watched.get("Watched per day").unwrap()
     );
 
     println!("\n---------- TIME SPENT DAILY \u{1F570}----------");
-    println!("Considering the fact that TikTok videos are on average 27.5 seconds long, and that you've watched {} videos a day,", statistics.watched.get("Watched per day").unwrap());
-    println!("we can estimate how much time you've spent on TikTok on average.");
-    println!("{}", statistics.time);
-    println!("Be aware of your screen time and of the way you consume content.");
+    println!("TikTok videos are on average 27.5 seconds long, so we can estimate how much time you spend on TikTok every day.");
+    println!(
+        "Time wasted on TikTok every day : {} on average",
+        statistics.time
+    );
+    println!("This stat is not 100% precise!");
 
     println!("\n---------- FAVORITES \u{1F60D} ----------");
     println!(
-        "You have {} favorite effects",
+        "{} favorite effects",
         statistics.favorites.get("Effects").unwrap()
     );
     println!(
-        "You have {} favorite hashtags",
+        "{} favorite hashtags",
         statistics.favorites.get("Hashtags").unwrap()
     );
     println!(
-        "You have {} favorite sounds",
+        "{} favorite sounds",
         statistics.favorites.get("Sounds").unwrap()
     );
     println!(
-        "You have {} favorite videos",
+        "{} favorite videos",
         statistics.favorites.get("Videos").unwrap()
     );
 
     println!("\n---------- LIKES \u{2764} ----------");
     println!(
-        "You have liked {} videos in the past {} days",
+        "You've liked {} videos in the last {} days",
         statistics.likes_left.get("Videos liked").unwrap(),
         statistics.likes_left.get("Days since oldest like").unwrap()
     );
     println!(
-        "You have liked {} videos a day on average\n",
+        "You've liked {} videos per day on average\n",
         statistics.likes_left.get("Likes per day").unwrap()
     );
     println!(
-        "You liked around {}% of the videos you have watched",
+        "You've liked {}% of the videos you've watched",
         statistics
             .likes_left
             .get("Liked videos percentage")
@@ -96,31 +99,31 @@ fn main() {
     );
 
     println!("\n---------- COMMENTS \u{1F4AC} ----------");
-    println!("You have posted {} comments", statistics.comments);
+    println!("You've posted {} comments", statistics.comments);
 
     println!("\n---------- DIRECT MESSAGES \u{2709} ----------");
     for entry in statistics.dms.iter() {
         println!("{} {} messages", entry.0, entry.1);
     }
     println!(
-        "\nYou sent {} messages in total",
+        "\nTotal number of messages : {}",
         statistics.dms.into_values().sum::<usize>()
     );
 
     println!("\n---------- AUDIENCE STATISTICS \u{1F464} ----------");
 
     if statistics.likes_received > 0 && statistics.videos_published == 0 {
-        println!("You have received {} likes but no videos were found in the data. You must have deleted old videos", statistics.likes_received);
+        println!("You've received {} likes but no videos were found in the data. You must have deleted old videos", statistics.likes_received);
     } else if statistics.likes_received == 0 && statistics.videos_published == 0 {
-        println!("You received no likes and published no videos");
+        println!("You haven't posted any videos or received any likes");
     } else if statistics.likes_received == 0 && statistics.videos_published > 0 {
         println!(
-            "You have published {} videos, but sadly no one liked them",
+            "You've published {} videos - sadly no one liked them",
             statistics.videos_published
         );
     } else {
         println!(
-            "You have received {} likes in {} videos",
+            "You've received {} likes with {} videos",
             statistics.likes_received, statistics.videos_published
         );
         println!(
@@ -128,5 +131,5 @@ fn main() {
             (statistics.likes_received / statistics.videos_published)
         );
     }
-    println!("Please note that likes received from old videos are still being counted.")
+    println!("Note that likes from old videos are still counted.")
 }
